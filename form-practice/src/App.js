dad1f666cc9d227fc46d406281520bdb66a8d9d6
Erik Rodriguez <perhapserik@gmail.com>
2021-03-11 13:27:00 -0800

    • Adds formik and yup as new dependencies • Creates basic form

--- form-practice/src/App.js
+++ form-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Forms from "./components/Forms.js";
 
 function App() {
   return (
     <div className="App">
       <header> Form Practice</header>
+      <Forms />
     </div>
   );
 }
